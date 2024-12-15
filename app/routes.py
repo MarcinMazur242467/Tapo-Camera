797a98216e9aafc053ad4a89ef2fb3b9b8f9bef9
Marcin Mazur <242467@edu.p.lodz.pl>
2024-12-15 14:41:18 +0100

    steering

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify
 from flask_socketio import emit
 import cv2
 import base64
@@ -6,11 +6,25 @@ import time
 from threading import Thread
 from . import socketio
 from flask_socketio import SocketIO
+from pytapo import Tapo
+from flask import request
+import json
+
+def load_config(config_file='config.json'):
+    with open(config_file, 'r') as file:
+        return json.load(file)
+
+config = load_config()
+
+camera_ip = config['host']
+camera_user = config['user']
+camera_password = config['password']
+camera_url = config['rtsp_url']
+
+camera = Tapo(camera_ip, camera_user, camera_password)
 
 socket = SocketIO()
-
 bp = Blueprint('main', __name__)
-camera_url = "rtsp://tapo1234:tapo1234@192.168.18.13/stream1"
 
 def capture_frames():
     cap = cv2.VideoCapture(camera_url)
@@ -52,8 +66,37 @@ def capture_frames():
         # Czekanie przed wysłaniem kolejnej klatki (30 FPS)
         socketio.sleep(0.033)  # ~30 FPS
     
+@bp.route('/move', methods=['POST'])
+def move_camera():
+    print("Headers:", request.headers)  # Logowanie nagłówków żądania
+    print("Data received:", request.data)  # Logowanie surowych danych
 
+    try:
+        data = request.get_json()
+        print("Parsed JSON:", data)  # Logowanie sparsowanego JSONa
+        
+        if not data or 'direction' not in data:
+            return jsonify({"error": "Missing 'direction' in request"}), 400
 
+        direction = data.get('direction')
+
+        if direction == 'left':
+            camera.moveMotor(-10, 0)  # Przesunięcie w lewo (x=-10, y=0)
+        elif direction == 'right':
+            camera.moveMotor(10, 0)   # Przesunięcie w prawo (x=10, y=0)
+        elif direction == 'up':
+            camera.moveMotor(0, 10)   # Przesunięcie w górę (x=0, y=10)
+        elif direction == 'down':
+            camera.moveMotor(0, -10)  # Przesunięcie w dół (x=0, y=-10)
+        else:
+            return jsonify({"error": "Invalid direction"}), 400
+
+        return jsonify({"status": "success", "direction": direction})
+    
+    except Exception as e:
+        print(f"Error in move_camera: {e}")  # Logowanie błędu na serwerze
+        return jsonify({"error": "Internal Server Error", "message": str(e)}), 500
+    
 @bp.route('/')
 def index():
     return render_template('index.html')
